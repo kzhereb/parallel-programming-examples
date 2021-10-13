b9ab1f762722a45318bf0805af1830a4e6792485
Kostiantyn Zhereb <zhereb@gmail.com>
2021-10-13 15:22:46 +0300

    Progress commit: trying to implement multithreaded version, not working

--- cpp/matrix_multiply.cpp
+++ cpp/matrix_multiply.cpp
@@ -8,6 +8,9 @@
 #include "doctest.h"
 
 #include <iostream>
+#include <vector>
+#include <thread>
+#include <cassert>
 
 int* create_matrix(std::size_t size) {
 	int* result = new int[size*size];
@@ -47,6 +50,46 @@ void matrix_multiply_sequential(std::size_t size, int* input_A, int* input_B, in
 	}
 }
 
+void multiply_on_single_thread(std::size_t size, int* input_A, int* input_B, int* output_C, std::size_t from, std::size_t to) {
+	for(std::size_t i = from; i < to; i++) {
+			for (std::size_t j = 0; j < size; j++) {
+				output_C[i*size+j] = 0;
+			}
+	}
+	for(std::size_t i = from; i < to; i++) {
+		for (std::size_t j = 0; j < size; j++) {
+			for (std::size_t k = 0; k < size; k++) {
+				output_C[i*size + j] += input_A[i*size + k] * input_B[k*size+j];
+			}
+		}
+	}
+}
+
+void matrix_multiply_threads(std::size_t size, int* input_A, int* input_B, int* output_C, unsigned thread_num = 0) {
+	unsigned thread_count = (thread_num == 0)? std::thread::hardware_concurrency() : thread_num;
+	assert(thread_count > 0);
+	//std::cout<<thread_count<<std::endl;
+
+	std::vector<std::thread> threads;
+	std::size_t block_size = size/thread_count;
+	std::size_t from = 0;
+	for (std::size_t i = 0; i< thread_count; i++) {
+		std::size_t to = from + block_size;
+		if (to > size) { to = size;}
+
+		threads.push_back(std::thread(multiply_on_single_thread,
+				size, input_A, input_B, output_C,
+				from, to));
+
+		to = from;
+	}
+
+	for (std::thread & th : threads) {
+		if (th.joinable()) { th.join(); }
+	}
+
+}
+
 TEST_CASE("matrix multiply") {
 	std::size_t size = 100;
 	int* A = create_matrix(size);
@@ -55,7 +98,12 @@ TEST_CASE("matrix multiply") {
 	init_diagonal(size, A, 5);
 	init_diagonal(size, B, 3);
 
-	matrix_multiply_sequential(size, A, B, C);
+	SUBCASE("sequential") {
+		matrix_multiply_sequential(size, A, B, C);
+	}
+	SUBCASE("threads") {
+		matrix_multiply_threads(size, A, B, C);
+	}
 
 	int* expected = create_matrix(size);
 	init_diagonal(size, expected, 15);
