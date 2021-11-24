ed611e44b7c43725a7669b819aa65576f9e7f3cb
Kostiantyn Zhereb <zhereb@gmail.com>
2021-11-24 15:20:00 +0200

    Fixed bug with block limits calculation, added test for double
    For some reason, the bug didn't manifest when working with int matrices
    TODO: investigate why

--- cpp/matrix.h
+++ cpp/matrix.h
@@ -8,7 +8,11 @@
 #ifndef MATRIX_H_
 #define MATRIX_H_
 
+#include <iostream>
+
 #include <thread>
+#include <vector>
+#include <cassert>
 
 template<typename T>
 T* create_matrix(std::size_t size) {
@@ -28,7 +32,9 @@ template<typename T>
 bool is_equal(std::size_t size, T* left, T* right) {
 	for(std::size_t i = 0; i < size; i++) {
 			for (std::size_t j = 0; j < size; j++) {
-				if (left[i*size+j] != right[i*size+j] ) { return false;}
+				if (left[i*size+j] != right[i*size+j] ) {
+					std::cout<<"i="<<i<<", j="<<j<<",left[i,j]="<<left[i*size+j]<<",right[i,j]="<<right[i*size+j]<<std::endl;
+					return false;}
 			}
 	}
 	return true;
@@ -50,6 +56,50 @@ void matrix_multiply_sequential(std::size_t size, T* input_A, T* input_B, T* out
 	}
 }
 
+template<typename T>
+void multiply_on_single_thread(std::size_t size, T* input_A, T* input_B, T* output_C, std::size_t from, std::size_t to) {
+	for(std::size_t i = from; i < to; i++) {
+			for (std::size_t j = 0; j < size; j++) {
+				output_C[i*size+j] = T(0);
+			}
+	}
+	for(std::size_t i = from; i < to; i++) {
+		for (std::size_t j = 0; j < size; j++) {
+			for (std::size_t k = 0; k < size; k++) {
+				output_C[i*size + j] += input_A[i*size + k] * input_B[k*size+j];
+			}
+		}
+	}
+}
+
+template<typename T>
+void matrix_multiply_threads(std::size_t size, T* input_A, T* input_B, T* output_C, unsigned thread_num = 0) {
+	unsigned thread_count = (thread_num == 0)? std::thread::hardware_concurrency() : thread_num;
+	assert(thread_count > 0);
+	//std::cout<<thread_count<<std::endl;
+
+	std::vector<std::thread> threads;
+	std::size_t block_size = size/thread_count;
+	std::size_t from = 0;
+	for (std::size_t i = 0; i< thread_count; i++) {
+		std::size_t to = from + block_size;
+		if (to > size) { to = size;}
+		if (i == thread_count-1) { to = size;}
+
+		//std::cout<<"i="<<i<<",from="<<from<<",to="<<to<<std::endl;
+
+		threads.push_back(std::thread(multiply_on_single_thread<T>,
+				size, input_A, input_B, output_C,
+				from, to));
+
+		from = to;
+	}
+
+	for (std::thread & th : threads) {
+		if (th.joinable()) { th.join(); }
+	}
+
+}
 
 
 #endif /* MATRIX_H_ */
--- cpp/matrix_multiply.cpp
+++ cpp/matrix_multiply.cpp
@@ -11,54 +11,13 @@
 #include "doctest.h"
 
 #include <iostream>
-#include <vector>
-#include <thread>
-
-#include <cassert>
 
 
-void multiply_on_single_thread(std::size_t size, int* input_A, int* input_B, int* output_C, std::size_t from, std::size_t to) {
-	for(std::size_t i = from; i < to; i++) {
-			for (std::size_t j = 0; j < size; j++) {
-				output_C[i*size+j] = 0;
-			}
-	}
-	for(std::size_t i = from; i < to; i++) {
-		for (std::size_t j = 0; j < size; j++) {
-			for (std::size_t k = 0; k < size; k++) {
-				output_C[i*size + j] += input_A[i*size + k] * input_B[k*size+j];
-			}
-		}
-	}
-}
 
-void matrix_multiply_threads(std::size_t size, int* input_A, int* input_B, int* output_C, unsigned thread_num = 0) {
-	unsigned thread_count = (thread_num == 0)? std::thread::hardware_concurrency() : thread_num;
-	assert(thread_count > 0);
-	//std::cout<<thread_count<<std::endl;
 
-	std::vector<std::thread> threads;
-	std::size_t block_size = size/thread_count;
-	std::size_t from = 0;
-	for (std::size_t i = 0; i< thread_count; i++) {
-		std::size_t to = from + block_size;
-		if (to > size) { to = size;}
-
-		threads.push_back(std::thread(multiply_on_single_thread,
-				size, input_A, input_B, output_C,
-				from, to));
-
-		from = to;
-	}
-
-	for (std::thread & th : threads) {
-		if (th.joinable()) { th.join(); }
-	}
-
-}
 
 TEST_CASE("matrix multiply") {
-	std::size_t size = 300;
+	std::size_t size = 295;
 	int* A = create_matrix<int>(size);
 	int* B = create_matrix<int>(size);
 	int* C = create_matrix<int>(size);
@@ -86,7 +45,45 @@ TEST_CASE("matrix multiply") {
 	int* expected = create_matrix<int>(size);
 	init_diagonal(size, expected, valueA*valueB);
 
-	CHECK(is_equal(size, C, expected));
+	CHECK(is_equal(size, expected, C ));
+
+	delete[] A;
+	delete[] B;
+	delete[] C;
+	delete[] expected;
+}
+
+
+TEST_CASE("matrix multiply - double") {
+	std::size_t size = 295;
+	double* A = create_matrix<double>(size);
+	double* B = create_matrix<double>(size);
+	double* C = create_matrix<double>(size);
+	double valueA = 12.34;
+	double valueB = 0.567;
+	init_diagonal(size, A, valueA);
+	init_diagonal(size, B, valueB);
+
+	SUBCASE("sequential") {
+
+		for(std::size_t run = 0; run < 10; run++) {
+			Timer timer{"sequential"};
+			matrix_multiply_sequential(size, A, B, C);
+		}
+	}
+	SUBCASE("threads") {
+
+		for(std::size_t run = 0; run < 10; run++) {
+			Timer timer{"threads"};
+			matrix_multiply_threads(size, A, B, C);
+		}
+	}
+
+
+	double* expected = create_matrix<double>(size);
+	init_diagonal(size, expected, valueA*valueB);
+
+	CHECK(is_equal(size, expected, C));
 
 	delete[] A;
 	delete[] B;
